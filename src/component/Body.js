24e102ed3f9cb857433c3f8d29a2b90dc45e53d1
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-06-30 23:08:17 +0545

    Applying search option on our website

--- src/component/Body.js
+++ src/component/Body.js
@@ -1,41 +1,71 @@
 import RestaurantCard from "./RestaurantCard";
 import RestaurantList from "../utils/restaurantList";
-import {useEffect, useState} from "react"
+import { useEffect, useState } from "react";
 import Shimmer from "./shimmer";
 
 const Body = () => {
+  const [Restaurants, setRestaurants] = useState([]);
+  const [filteredRestaurant, setFilteredRestaurant] = useState([]);
 
-  const [Restaurants, setRestaurants] = useState([])
+  const [searchText, setSearchText] = useState("");
 
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    const data = await fetch(
+      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
+    );
     const json = await data.json();
     console.log(json);
     // console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
     // Optional Chaining
     // setRestaurants(json?.data?.cards[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
-    const restaurants = json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle?.restaurants || [];
+    const restaurants =
+      json?.data?.cards?.[1]?.card?.card?.gridElements?.infoWithStyle
+        ?.restaurants || [];
 
     setRestaurants(restaurants);
-  }
+    setFilteredRestaurant(restaurants);
+  };
 
-  return Restaurants.length === 0?(
-     <Shimmer />):(
+  return Restaurants.length === 0 ? (
+    <Shimmer />
+  ) : (
     <div className="body">
       <div className="filter">
+        <div className="search">
+          <input
+            type="text"
+            className="search-box"
+            value={searchText}
+            onChange={(e) => {
+              // e.preventDefault();
+              setSearchText(e.target.value);
+            }}
+          />
+          <button
+            onClick={() => {
+              //Filter the restraunt cards and update the UI
+              //searchText
+              console.log(searchText);
+
+              const filterRestaurant = Restaurants.filter((res) =>
+                res.info.name.toLowerCase().includes(searchText.toLowerCase())
+              );
+              setFilteredRestaurant(filterRestaurant);
+            }}
+          >
+            Search
+          </button>
+        </div>
         <button
           className="filter-btn"
           onClick={() => {
-            filterList= Restaurants.filter(
-              (res) => res.info.avgRating >4
-            );
+            filterList = Restaurants.filter((res) => res.info.avgRating > 4);
             setRestaurants(filterList);
           }}
-
           onChange={setRestaurants}
         >
           Top Rated Restaurants
@@ -45,10 +75,10 @@ const Body = () => {
       <div className="res-container">
         {/* {" "} */}
         {/* res=  restaurant*/}
-        {Restaurants.map((restaurant) => {
-          console.log({restaurant})
+        {filteredRestaurant.map((restaurant) => {
+          console.log({ restaurant });
           return (
-            <RestaurantCard key={restaurant.info.id}  resData={restaurant} />
+            <RestaurantCard key={restaurant.info.id} resData={restaurant} />
           );
         })}
         ;
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,26 +1,37 @@
+import { useState } from "react";
 import { Header_LOGO_URL } from "../utils/constants";
 
 // Header Component
+let btnName = "LOGIN"
 const Header = () => {
-    return (
-      <div className="header">
-        <div className="logo-container">
-          <img
-            className="logo"
-            src={Header_LOGO_URL}
-          ></img>
-        </div>
+  const [BtnName, setBtnName] = useState(btnName);
   
-        <div className="nav-items">
-          <ul>
-            <li>Home</li>
-            <li>About Us</li>
-            <li>Contact</li>
-            <li>Cart</li>
-          </ul>
-        </div>
-      </div>
-    );
-  };
+  let btnChange = (() =>{
+    if(BtnName == "LOGIN"){
+      setBtnName("LOGOUT");
+    }
+    else{
+      setBtnName("LOGIN");
+    }
+  });
 
-  export default Header;
+  return (
+    <div className="header">
+      <div className="logo-container">
+        <img className="logo" src={Header_LOGO_URL}></img>
+      </div>
+
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact</li>
+          <li>Cart</li>
+          <button onClick={btnChange}>{BtnName}</button>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
