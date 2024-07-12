f35a6be561691a1d7a73e9f89fe1eb508f6cdb66
Abhinandan-Sah <abhinandansah222@gmail.com>
2024-07-12 23:52:13 +0545

    updated shimmer.js component

--- src/component/Shimmer.js
+++ src/component/Shimmer.js
@@ -1,5 +1,7 @@
 const Shimmer = () => {
-    return (<div className="flex flex-wrap justify-center shimmer-container mt-[12rem]">
+    return (<div>
+        <div className="w-10/12 m-auto text-center text-wrap font-bold text-2xl mt-28">Since, this project works on Swiggy API we cannot directly use it. So, Please Download Allow CORS Extension - Click on this <a href="https://chromewebstore.google.com/detail/allow-cors-access-control/lhobafahddgcelffkeicbaginigeejlf?hl=en" className="text-blue-500">LINK</a> to redirect to the Extension Page. </div>
+        <div className="flex flex-wrap justify-center shimmer-container mt-6">      
         <div className="m-4 p-4 w-[250px] h-[450px] rounded-md bg-gray-100 shimmer-card">Cards</div>
         <div className="m-4 p-4 w-[250px] h-[450px] rounded-md bg-gray-100 shimmer-card">Cards</div>
         <div className="m-4 p-4 w-[250px] h-[450px] rounded-md bg-gray-100 shimmer-card">Cards</div> 
@@ -12,6 +14,8 @@ const Shimmer = () => {
         <div className="m-4 p-4 w-[250px] h-[450px] rounded-md bg-gray-100 shimmer-card">Cards</div> 
 
     </div>
+    </div>
+    
     );
 };
 
