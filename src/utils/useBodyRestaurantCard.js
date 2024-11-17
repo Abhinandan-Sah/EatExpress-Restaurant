966671ab2a9b06ce410807692774112f6d842caa
Abhinandan Sah <abhinandansah222@gmail.com>
2024-11-17 18:38:40 +0530

    updated hard coded data

--- src/utils/useBodyRestaurantCard.js
+++ src/utils/useBodyRestaurantCard.js
@@ -1,20 +1,22 @@
 import { useState, useEffect } from "react";
+import dataDb from "./data.js";
 
 const useBodyRestaurantCard = () => {
   const [listOfRestaurants, setListOfRestaurants] = useState([]);
   const [filteredRestaurant, setFilteredRestaurant] = useState([]);
-
+console.log(dataDb);
   useEffect(() => {
     fetchData();
   }, []);
 
   const fetchData = async () => {
-    const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
-
+    // const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=19.0759837&lng=72.8776559&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+    // const data = await fetch(dataDb);
       // "https://www.swiggy.com/dapi/restaurants/list/v5?lat=25.5940947&lng=85.1375645&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
 
-    
-    const json = await data.json();
+    // const json = await data.json();
+    const json = dataDb;
+
     // console.log(json);
     // console.log(json.data.cards[2].card.card.gridElements.infoWithStyle.restaurants);
     // Optional Chaining
